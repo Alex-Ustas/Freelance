1cdcae9d3312405ce836fed07a2321bebf7e0c5a
Alex Ustas <alex.ustas@internet.ru>
2023-01-11 13:29:03 +0300

    fix model_freelance due to page content was changed

--- model_freelance.py
+++ model_freelance.py
@@ -2,7 +2,84 @@
 
 import requests
 from bs4 import BeautifulSoup as bs
-import re
+
+
+def parse_project(tasks, project: dict):
+    for task in tasks:
+        title_part = task.find('div', class_='row')
+        footer_part = task.find('div', class_='project-footer')
+
+        task_id = '0'
+        title = '[Title not defined]'
+        info = ''
+        cost = ''
+        time = ''
+        resp = ''
+        term = ''
+        link = ''
+        if title_part is not None:
+            title = title_part.find('h2', class_='title')
+            if title is None:
+                title = '[Title not defined]'
+            else:
+                title = title.get('title').strip()
+
+            task_id = title_part.find('h2', class_='title')
+            if task_id is None:
+                task_id = '0'
+            else:
+                task_id = task_id.find('a').get('href')[:-5]
+                task_id = task_id[task_id.rfind('-') + 1:]
+            # print(task_id, title)
+
+            info = title_part.find('a', class_='description')
+            if info is None:
+                info = ''
+            else:
+                info = info.next.strip().replace('\n', ' ').replace('\r', ' ').replace('  ', ' ')
+            # print(info)
+
+            cost = title_part.find('div', class_='cost')
+            if cost is None:
+                cost = ''
+            else:
+                cost = cost.next.strip().lower()
+            # print(cost)
+
+            term = title_part.find('div', class_='term')
+            if term is None:
+                term = ''
+            else:
+                term = term.find('span')
+                if term is None:
+                    term = ''
+                else:
+                    term = 'Срок выполнения: ' + term.next.strip()
+            # print(term)
+
+            link = title_part.find('a', class_='description')
+            if link is None:
+                link = ''
+            else:
+                link = 'https://freelance.ru' + link.get('href')
+            # print(link)
+
+        if footer_part is not None:
+            time = footer_part.find('time', class_='timeago')
+            if time is None:
+                time = ''
+            else:
+                time = 'Опубликовано: ' + time.next.strip()
+            # print(time)
+
+            resp = footer_part.find('span', class_='comments-count')
+            if resp is None:
+                resp = ''
+            else:
+                resp = resp.next.strip()
+            # print(resp)
+
+        project[task_id] = ['Freelance', title, info, cost, time, resp, term, link]
 
 
 def parse_freelance(method=1) -> (dict, str):
@@ -14,7 +91,7 @@ def parse_freelance(method=1) -> (dict, str):
         headers = \
             {
                 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
-                 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0'
+                 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
             }
         try:
             rq = requests.get(href, headers=headers)
@@ -22,76 +99,17 @@ def parse_freelance(method=1) -> (dict, str):
         except Exception as err:
             return data, f'Freelance: unexpected {err=}, {type(err)=}\n'
     else:
-        html = open(r'C:\Temp\Python\freelance2.html', encoding='utf8').read()
+        html = open(r'C:\Temp\Python\freelance3.html', encoding='utf8').read()
         soup = bs(html, 'html.parser')
 
     content = soup.find('div', class_='projects m-t-2')
     if content is None:
-        return data, f'FL: status={rq.status_code}, reason={rq.reason}\n'
+        return data, f'Freelance: empty content,\nstatus={rq.status_code}, reason={rq.reason}\n'
 
-    tasks = content.find_all('div', class_=re.compile('box-shadow project'))
-
-    for task in tasks:
-        title_part = task.find('div', class_='box-title')
-        footer_part = task.find('div', class_='box-info')
-
-        title = title_part.find('h2', class_='title')
-        if title is None:
-            title = '[Title not defined]'
-        else:
-            title = title.get('title')
-
-        task_id = title_part.find('h2', class_='title')
-        if task_id is None:
-            task_id = '0'
-        else:
-            task_id = task_id.find('a').get('href')[:-5]
-            task_id = task_id[task_id.rfind('-') + 1:]
-        # print(task_id, title)
-
-        info = title_part.find('a', class_='description')
-        if info is None:
-            info = ''
-        else:
-            info = info.next.strip().replace('\n', ' ').replace('\r', ' ').replace('  ', ' ')
-        # print(info)
-
-        cost = footer_part.find('div', class_='cost')
-        if cost is None:
-            cost = ''
-        else:
-            cost = cost.next.strip()
-        # print(cost)
-
-        time = title_part.find('time', class_='timeago')
-        if time is None:
-            time = ''
-        else:
-            time = time.next.strip()
-        # print(time)
-
-        resp = title_part.find('i', class_='fa fa-comments-o')
-        if resp is None:
-            resp = ''
-        else:
-            resp = resp.next.replace('Откликов:', '').strip()
-        # print(resp)
-
-        term = footer_part.find('div', class_='term')
-        if term is None:
-            term = ''
-        else:
-            term = term.next.strip()
-        # print(term)
-
-        link = title_part.find('a', class_='description')
-        if link is None:
-            link = ''
-        else:
-            link = 'https://freelance.ru' + link.get('href')
-        # print(link)
-
-        data[task_id] = ['Freelance', title, info, cost, time, resp, term, link]
+    tasks = content.find_all('div', class_='project')
+    parse_project(tasks, data)
+    tasks = content.find_all('div', class_='project highlight')
+    parse_project(tasks, data)
 
     return data, ''
 
